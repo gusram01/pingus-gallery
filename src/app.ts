5d10dea4b5dc2d26ed3bbb6b6fda5e04a64021a2
Gus Ramírez <g.ram.bt@hotmail.com>
2021-10-15 23:13:43 -0500

    build: add cors, helmet, morgan, multer

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
+import path from 'path';
 import express from 'express';
+import morgan from 'morgan';
+import multer, { Options as MulterOptions } from 'multer';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
+const uploadsPath = path.resolve(__dirname, 'public', 'uploads');
+const multerConfig: MulterOptions = {
+  dest: uploadsPath
+};
+
 app.set('port', process.env.PORT);
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(multer(multerConfig).single('image'));
+app.use(helmet());
+app.use(cors());
+
+app.get('/', (_, res) => {
+  return res.json({ ok: true });
+});
 
 export default app;
